823aaec6fb842f76452e1af400a285e03127607e
jeanpablovm <jeanpablo28@gmail.com>
2024-10-16 18:20:26 -0600

    Update __manifest__.py
    update the version

--- cr_electronic_invoice_pos/__manifest__.py
+++ cr_electronic_invoice_pos/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Facturación electrónica Costa Rica POS',
-    'version': '15.0.1.0.0',
+    'version': '17.0.1.0.0',
     'author': 'Odoo CR, TechMicro Inc S.A.',
     'license': 'OPL-1',
     'website': 'http://www.techmicrocr.com',
